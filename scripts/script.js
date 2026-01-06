e9565d198ba468a508800d60e66d8d9ca2915b68
Braz Junior <90149109+brxzjunior@users.noreply.github.com>
2026-01-05 22:18:30 -0400

    aula 474 - Reordenando notas fixas

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,23 @@ const addNoteBtn = document.querySelector(".add-note");
 
 
 //Functions
+function showNotes(){
+    cleanNotes();
+
+    getNotes().forEach((note) => {
+        const noteElement = createNote(note.id, note.content, note.fixed);
+        notesContainer.appendChild(noteElement);
+    });
+};
+
+function cleanNotes(){
+    notesContainer.replaceChildren([]);
+}
+
+
 function addNote() {
+    const notes = getNotes();
+
     const noteObject = {
         id: generateId(),
         content: noteInput.value,
@@ -17,6 +33,12 @@ function addNote() {
     const noteElement = createNote(noteObject.id, noteObject.content);
     
     notesContainer.appendChild(noteElement);
+    
+    notes.push(noteObject);
+
+    saveNotes(notes);
+
+    noteInput.value = "";
 }
 
 function generateId() {
@@ -36,9 +58,53 @@ function createNote(id, content, fixed){
 
         element.appendChild(textarea);
 
+        const pinIcon = document.createElement("i");
+
+        pinIcon.classList.add(...["bi","bi-pin"]);
+
+        element.appendChild(pinIcon);
+
+        if(fixed) {
+            element.classList.add("fixed");
+        };
+
+        //Eventos do elemento
+        element.querySelector(".bi-pin").addEventListener("click", () => {
+            toggleFixNote(id);
+        });
+
         return element;
-}
+};
+
+function toggleFixNote(id){
+    const notes = getNotes();
+
+    const targetNote = notes.filter((note) => note.id === id)[0];
+ 
+    targetNote.fixed = !targetNote.fixed;
+
+    saveNotes(notes);
+
+    showNotes();
+};
+
+//LocalStorage
+function getNotes(){
+    const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+    
+    const orderedNotes = notes.sort((a, b) => a.fixed > b.fixed ? -1 : 1);
+    
+    return orderedNotes;
+};
+
+
+function saveNotes(notes){
+    localStorage.setItem("notes", JSON.stringify(notes));
+};
 
 
 //Events
 addNoteBtn.addEventListener("click", ()=> addNote());
+
+//Inicialization
+showNotes();
